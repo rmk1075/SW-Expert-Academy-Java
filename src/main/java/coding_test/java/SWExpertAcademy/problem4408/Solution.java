d856075753a9cb50b784f121e1c4593b3248d431
jamie <rmk1075@gmail.com>
2019-07-22 23:19:34 +0900

    problem 4408 pass

--- src/main/java/coding_test/java/SWExpertAcademy/problem4408/Solution.java
+++ src/main/java/coding_test/java/SWExpertAcademy/problem4408/Solution.java
@@ -1,46 +1,44 @@
 package coding_test.java.SWExpertAcademy.problem4408;
 
-import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Solution {
 	static int T;
 	static int N;
-	static ArrayList<Integer[]> arr;
-	static boolean path[];
+	static int[] path;
 
 	public static void main(String[] args) {
-		/*
-		 * arr[N][2] = {start_room, destination_room}
-		 * start && dest <= 400
-		 */
 		Scanner sc = new Scanner(System.in);
 		
 		T = sc.nextInt();
 		for(int i = 0; i < T; i++) {
 			N = sc.nextInt();
+			path = new int[201];
 			
-			Integer[] value = new Integer[3];
 			for(int j = 0; j < N; j++) {
-				value[1] = sc.nextInt();
-				value[2] = sc.nextInt();
-				value[0] = Math.abs(value[1] - value[2]);
+				int x = sc.nextInt();
+				int y = sc.nextInt();
 				
-				arr.add(value);
+				if(x%2 == 1) x++;
+				if(y%2 == 1) y++;
+				
+				int temp;
+				if(y < x) {
+					temp = x;
+					x = y;
+					y = temp;
+				}
+				
+				for(int k = x/2; k <= y/2; k++) {
+					path[k]++;
+				}
 			}
+
+			Arrays.sort(path);			
+			int result = path[200];
 			
-			//sort arr
-			
-			/*
-			 * for loop: i = 0 ~ length()
-			 * path 채우기
-			 * 	for loop: i = 1 ~ length()
-			 * 		path check하면서 count++
-			 */
-			
-			int count = 0;
-			
-			System.out.println("#" + (i+1) + " " + count);
+			System.out.println("#" + (i+1) + " " + result);
 		}
 	}
 }
