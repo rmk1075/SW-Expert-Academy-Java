3c3797040d45a85fe213d2ae6674d9f7f8f6341b
jamie <rmk1075@gmail.com>
2019-07-29 22:53:13 +0900

    problem 6782 pass

--- src/main/java/coding_test/java/SWExpertAcademy/problem6782/Solution.java
+++ src/main/java/coding_test/java/SWExpertAcademy/problem6782/Solution.java
@@ -4,24 +4,25 @@ import java.util.Scanner;
 
 public class Solution {
 	static int T;
-	static int N;
+	static long N;
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		T = sc.nextInt();
 		
 		for(int t = 0; t < T; t++) {
-			N = sc.nextInt();
+			N = sc.nextLong();
 			int count = 0;
 			double temp = 0.0;
-						
+            
 			while(N != 2) {
 				temp = Math.sqrt(N);
-
-				if((2 < N) && ((temp - (int)Math.sqrt(N) == 0))) {
-					N = (int)Math.sqrt(N);
+				if((int)temp == temp) {
+					N = (int)temp;
 				} else {
-					N++;
+					temp = (int)temp;
+					count += ((temp+1) * (temp+1) - N);
+					N = (int)Math.sqrt((temp + 1) * (temp + 1));
 				}
 				
 				count++;
