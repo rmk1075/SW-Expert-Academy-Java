4350a22231652118d3ad76c9995189de88fcd892
jamie <rmk1075@gmail.com>
2020-02-28 13:50:13 +0900

    ssafy_algo_2115_java

--- ssafy/2115/Solution.java
+++ ssafy/2115/Solution.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Solution {
@@ -9,13 +8,14 @@ public class Solution {
     static int[][] m;
     static int[][] map;
     static boolean[][] visited;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
         T = Integer.parseInt(br.readLine());
-        for(int t = 1; t <= T; t++) {
+        for (int t = 1; t <= T; t++) {
             st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
             M = Integer.parseInt(st.nextToken());
@@ -23,16 +23,16 @@ public class Solution {
             map = new int[N][N];
             visited = new boolean[N][N];
 
-            for(int i = 0; i < N; i++) {
+            for (int i = 0; i < N; i++) {
                 st = new StringTokenizer(br.readLine());
-                for(int j = 0; j < N; j++) {
+                for (int j = 0; j < N; j++) {
                     map[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
             maxProfit = 0;
             m = new int[2][M];
-            choice(0, 0);
-            
+            choice(0, 0, 0);
+
             sb.append("#" + t + " " + maxProfit + "\n");
         }
 
@@ -40,46 +40,120 @@ public class Solution {
     }
 
     public static int cal(int index, int count, int sum, int sumC) {
-        if(C <= sumC || count == m[index].length) return sum;
+        if (C <= sumC || count == M) return sum;
 
-        int result = Math.max(sum, cal(index, count+1, sum, sumC));
+        int result = Math.max(sum, cal(index, count + 1, sum, sumC));
 
-        if(sumC + m[index][count] <= C) result = Math.max(result, cal(index, count + 1, sum + m[index][count] * m[index][count], sumC + m[index][count]));
+        if (sumC + m[index][count] <= C) result = Math.max(result, cal(index, count + 1, sum + m[index][count] * m[index][count], sumC + m[index][count]));
         return result;
     }
 
-    public static void choice(int row, int count) {
+    public static void choice(int row, int col, int count) {
 
-        if(count == 2) {
-            int sum = 0;
-            sum = cal(0, 0, 0, 0) + cal(1, 0, 0, 0);
-
-            maxProfit = Math.max(maxProfit, sum);
-            return ;
+        if (count == 2) {
+            maxProfit = Math.max(maxProfit, cal(0, 0, 0, 0) + cal(1, 0, 0, 0));
+            return;
         }
 
-        for(int i = row; i < N; i++) {
-            for(int j = 0; j < N; j++) {
-                if(visited[i][j]) continue;
-                
-                boolean isOk = true;
-                for(int k = 1; k < M; k++) {
-                    if((-1 < j-k && visited[i][j-k]) || (j+k == N || visited[i][j+k])) {
-                        isOk = false;
-                        break;
-                    }
-                }
+        for (int i = row; i < N; i++) {
+            int c = (i == row) ? col : 0;
+            for (int j = c; j <= N-M; j++) {
+                if (visited[i][j]) continue;
 
-                if(isOk) {
-                    visited[i][j] = true;
-                    for (int k = 0; k < M; k++) {
-                        m[count][k] = map[i][j+k];
-                    }
-                    choice(i, count+1);
-
-                    visited[i][j] = false;
+                visited[i][j] = true;
+                for (int k = 0; k < M; k++) {
+                    m[count][k] = map[i][j + k];
                 }
+                choice(i, j+M, count + 1);
+
+                visited[i][j] = false;
             }
         }
     }
 }
+
+// import java.io.BufferedReader;
+// import java.io.IOException;
+// import java.io.InputStreamReader;
+// import java.util.Arrays;
+// import java.util.StringTokenizer;
+
+// public class Solution {
+//     static int T, N, M, C, maxProfit = 0;
+//     static int[][] m;
+//     static int[][] map;
+//     static boolean[][] visited;
+//     public static void main(String[] args) throws IOException {
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         StringBuilder sb = new StringBuilder();
+//         StringTokenizer st;
+
+//         T = Integer.parseInt(br.readLine());
+//         for(int t = 1; t <= T; t++) {
+//             st = new StringTokenizer(br.readLine());
+//             N = Integer.parseInt(st.nextToken());
+//             M = Integer.parseInt(st.nextToken());
+//             C = Integer.parseInt(st.nextToken());
+//             map = new int[N][N];
+//             visited = new boolean[N][N];
+
+//             for(int i = 0; i < N; i++) {
+//                 st = new StringTokenizer(br.readLine());
+//                 for(int j = 0; j < N; j++) {
+//                     map[i][j] = Integer.parseInt(st.nextToken());
+//                 }
+//             }
+//             maxProfit = 0;
+//             m = new int[2][M];
+//             choice(0, 0);
+            
+//             sb.append("#" + t + " " + maxProfit + "\n");
+//         }
+
+//         System.out.println(sb.toString());
+//     }
+
+//     public static int cal(int index, int count, int sum, int sumC) {
+//         if(C <= sumC || count == m[index].length) return sum;
+
+//         int result = Math.max(sum, cal(index, count+1, sum, sumC));
+
+//         if(sumC + m[index][count] <= C) result = Math.max(result, cal(index, count + 1, sum + m[index][count] * m[index][count], sumC + m[index][count]));
+//         return result;
+//     }
+
+//     public static void choice(int row, int count) {
+
+//         if(count == 2) {
+//             int sum = 0;
+//             sum = cal(0, 0, 0, 0) + cal(1, 0, 0, 0);
+
+//             maxProfit = Math.max(maxProfit, sum);
+//             return ;
+//         }
+
+//         for(int i = row; i < N; i++) {
+//             for(int j = 0; j < N; j++) {
+//                 if(visited[i][j]) continue;
+                
+//                 boolean isOk = true;
+//                 for(int k = 1; k < M; k++) {
+//                     if((-1 < j-k && visited[i][j-k]) || (j+k == N || visited[i][j+k])) {
+//                         isOk = false;
+//                         break;
+//                     }
+//                 }
+
+//                 if(isOk) {
+//                     visited[i][j] = true;
+//                     for (int k = 0; k < M; k++) {
+//                         m[count][k] = map[i][j+k];
+//                     }
+//                     choice(i, count+1);
+
+//                     visited[i][j] = false;
+//                 }
+//             }
+//         }
+//     }
+// }
