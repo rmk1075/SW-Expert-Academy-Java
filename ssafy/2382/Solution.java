520bc812ac9bc26aa2ee79471b337f07c00353f9
jamie <rmk1075@gmail.com>
2020-04-09 21:19:58 +0900

    2382_array

--- ssafy/2382/Solution.java
+++ ssafy/2382/Solution.java
@@ -1,93 +1,179 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 class Virus implements Comparable<Virus> {
-    int i, j, num, cnt, dir;
-    
-    Virus(int num, int i, int j, int cnt, int dir) {
+    int x, y, num, d;
+    boolean death = false;
+
+    Virus(int x, int y, int num, int d) {
+        this.x = x;
+        this.y = y;
         this.num = num;
-        this.i = i;
-        this.j = j;
-        this.cnt = cnt;
-        this.dir = dir;
+        this.d = d;
     }
 
+    @Override
     public int compareTo(Virus o) {
-        if (this.num == o.num) {
-            return o.cnt - this.cnt;
-        }
         return this.num - o.num;
     }
-
 }
 
 public class Solution {
-    static int N, M, K;
-    static int di[] = {-1, 1, 0, 0 }, dj[] = {0, 0, -1, 1 };
-    static List<Virus> list;
-
+    static int T, N, M, K, map[][]; // N: num of cells, M: time, K: num of viruses
+    static int[] dx = {-1, 1, 0, 0}, dy = {0, 0, -1, 1};
+    static Virus[] viruses;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int TC = Integer.parseInt(br.readLine());
-
-        for (int tc = 1; tc <= TC; tc++) {
+        T = Integer.parseInt(br.readLine());
+        for(int t = 1; t <= T; t++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
             M = Integer.parseInt(st.nextToken());
             K = Integer.parseInt(st.nextToken());
+            map = new int[N][N];
+            viruses = new Virus[K];
 
-            list = new ArrayList();
-            for (int k = 0; k < K; k++) {
+            for(int i = 0; i < N; i++) Arrays.fill(map[i], -1);
+            for(int i = 0; i < K; i++) {
                 st = new StringTokenizer(br.readLine());
-                int i = Integer.parseInt(st.nextToken());
-                int j = Integer.parseInt(st.nextToken());
-                list.add(new Virus(i * N + j, i, j, Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()) - 1));
+                int x = Integer.parseInt(st.nextToken()), y = Integer.parseInt(st.nextToken()), num = Integer.parseInt(st.nextToken());
+                viruses[i] = new Virus(x, y, num, Integer.parseInt(st.nextToken())-1);
             }
+            Arrays.sort(viruses);
 
-            for (int time = 0; time < M; time++) {
-                for (int idx = 0; idx < list.size(); idx++) {
-                    Virus virus = list.get(idx);
-                    virus.i = virus.i + di[virus.dir];
-                    virus.j = virus.j + dj[virus.dir];
-                    virus.num = (virus.i * N) + virus.j;
+            int x, y, d, cnt = 0;
+            while(0 < M-- && cnt < K) {
+                for(int i = 0; i < K; i++) {
+                    if(viruses[i].death) continue;
 
-                    if (virus.i == 0 || virus.j == 0 || virus.i == N - 1 || virus.j == N - 1) {
-                        virus.cnt /= 2;
-                        virus.dir = (virus.dir % 2 == 0) ? virus.dir+1 : virus.dir-1;
-                        if (virus.cnt == 0) {
-                            list.remove(idx);
-                            idx--;
-                        }
+                    d = viruses[i].d;
+                    x = viruses[i].x + dx[d];
+                    y = viruses[i].y + dy[d];
+                    if(x == 0 || y == 0 || x == N-1 || y == N-1) {
+                        viruses[i].num /= 2;
+                        viruses[i].d = (d % 2 == 0) ? d+1 : d-1;
                     }
-                }
 
-                Collections.sort(list);
-                for (int idx = 0; idx < list.size() - 1; idx++) {
-                    Virus now = list.get(idx);
-                    Virus next = list.get(idx + 1);
-                    if (now.num == next.num) {
-                        now.cnt += next.cnt;
-                        list.remove(idx + 1);
-                        idx--;
+                    viruses[i].x = x;
+                    viruses[i].y = y;
+                    if(map[x][y] != -1) {
+                        viruses[i].num += viruses[map[x][y]].num;
+                        viruses[map[x][y]].death = true;
+                        cnt++;
                     }
-                }
-            }
 
-            int total = 0;
-            for (int p = 0; p < list.size(); p++) {
-                total += list.get(p).cnt;
+                    map[x][y] = i;
+                }
+                for(int i = 0; i < N; i++) Arrays.fill(map[i], -1);
+                Arrays.sort(viruses);
             }
-            sb.append("#" + tc + " " + total + "\n");
+            
+            int ans = 0;
+            for(int i = 0; i < K; i++) {
+                if(viruses[i].death) continue;
+                ans += viruses[i].num;
+            }
+            sb.append("#" + t + " " + ans + "\n");
         }
-        System.out.println(sb.toString());
+        System.out.println(sb);
     }
 }
+
+
+// import java.io.BufferedReader;
+// import java.io.IOException;
+// import java.io.InputStreamReader;
+// import java.util.ArrayList;
+// import java.util.Collections;
+// import java.util.List;
+// import java.util.StringTokenizer;
+
+// class Virus implements Comparable<Virus> {
+//     int i, j, num, cnt, dir;
+    
+//     Virus(int num, int i, int j, int cnt, int dir) {
+//         this.num = num;
+//         this.i = i;
+//         this.j = j;
+//         this.cnt = cnt;
+//         this.dir = dir;
+//     }
+
+//     public int compareTo(Virus o) {
+//         if (this.num == o.num) {
+//             return o.cnt - this.cnt;
+//         }
+//         return this.num - o.num;
+//     }
+
+// }
+
+// public class Solution {
+//     static int N, M, K;
+//     static int di[] = {-1, 1, 0, 0 }, dj[] = {0, 0, -1, 1 };
+//     static List<Virus> list;
+
+//     public static void main(String[] args) throws IOException {
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         StringBuilder sb = new StringBuilder();
+//         int TC = Integer.parseInt(br.readLine());
+
+//         for (int tc = 1; tc <= TC; tc++) {
+//             StringTokenizer st = new StringTokenizer(br.readLine());
+//             N = Integer.parseInt(st.nextToken());
+//             M = Integer.parseInt(st.nextToken());
+//             K = Integer.parseInt(st.nextToken());
+
+//             list = new ArrayList();
+//             for (int k = 0; k < K; k++) {
+//                 st = new StringTokenizer(br.readLine());
+//                 int i = Integer.parseInt(st.nextToken());
+//                 int j = Integer.parseInt(st.nextToken());
+//                 list.add(new Virus(i * N + j, i, j, Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()) - 1));
+//             }
+
+//             for (int time = 0; time < M; time++) {
+//                 for (int idx = 0; idx < list.size(); idx++) {
+//                     Virus virus = list.get(idx);
+//                     virus.i = virus.i + di[virus.dir];
+//                     virus.j = virus.j + dj[virus.dir];
+//                     virus.num = (virus.i * N) + virus.j;
+
+//                     if (virus.i == 0 || virus.j == 0 || virus.i == N - 1 || virus.j == N - 1) {
+//                         virus.cnt /= 2;
+//                         virus.dir = (virus.dir % 2 == 0) ? virus.dir+1 : virus.dir-1;
+//                         if (virus.cnt == 0) {
+//                             list.remove(idx);
+//                             idx--;
+//                         }
+//                     }
+//                 }
+
+//                 Collections.sort(list);
+//                 for (int idx = 0; idx < list.size() - 1; idx++) {
+//                     Virus now = list.get(idx);
+//                     Virus next = list.get(idx + 1);
+//                     if (now.num == next.num) {
+//                         now.cnt += next.cnt;
+//                         list.remove(idx + 1);
+//                         idx--;
+//                     }
+//                 }
+//             }
+
+//             int total = 0;
+//             for (int p = 0; p < list.size(); p++) {
+//                 total += list.get(p).cnt;
+//             }
+//             sb.append("#" + tc + " " + total + "\n");
+//         }
+//         System.out.println(sb.toString());
+//     }
+// }
 
 // import java.io.BufferedReader;
 // import java.io.IOException;
