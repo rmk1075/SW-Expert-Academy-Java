ff6376635d71b817f4d2fe9c8953d160d934876f
jamie <rmk1075@gmail.com>
2020-04-11 04:04:45 +0900

    2112_update

--- ssafy/2112/Solution.java
+++ ssafy/2112/Solution.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-    static int T, D, W, K, ans, map[][];
+    static int T, D, W, K, ans, map[][], tMap[][];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
@@ -16,9 +16,10 @@ public class Solution {
             W = Integer.parseInt(st.nextToken());
             K =Integer.parseInt(st.nextToken());
             map = new int[D][W];
+            tMap = new int[D][W];
             for(int i = 0; i < D; i++) {
                 st = new StringTokenizer(br.readLine());
-                for(int j = 0; j < W; j++) map[i][j] = Integer.parseInt(st.nextToken());
+                for(int j = 0; j < W; j++) map[i][j] = tMap[i][j] = Integer.parseInt(st.nextToken());
             }
 
             if(K == 1) {
@@ -50,49 +51,46 @@ public class Solution {
 
             ans = D;
             for(int i = 0; i < D; i++) {
-                find(i, 0, 1, map);
-                find(i, 1, 1, map);
+                for(int j = 0; j < W; j++) tMap[i][j] = 0;
+                find(i, 1);
+                for(int j = 0; j < W; j++) tMap[i][j] = map[i][j];
             }
-
             sb.append("#" + t + " " + ans + "\n");
         }
         System.out.println(sb);
     }
 
-    public static void find(int d, int val, int count, int[][] field) {
+    public static void find(int d, int count) {
         if(ans <= count) return ;
+        for(int val = 0; val < 2; val++) {
+            if(val == 1) for(int j = 0; j < W; j++) tMap[d][j] = 1;
 
-        int[][] tMap = new int[D][W];
-        for(int i = 0; i < D; i++) {
-            if(i == d) for(int j = 0; j < W; j++) tMap[i][j] = val;
-            else for(int j = 0; j < W; j++) tMap[i][j] = field[i][j];
-        }
-
-        boolean isOk = false;
-        for(int i = 0; i < W; i++) {
-            isOk = false;
-            int cnt = 1;
-            for(int j = 1; j < D; j++) {
-                if(tMap[j-1][i] == tMap[j][i]) cnt++;
-                else cnt = 1;
-
-                if(cnt == K) {
-                    isOk = true;
-                    break;
+            boolean isOk = false;
+            for(int i = 0; i < W; i++) {
+                isOk = false;
+                int cnt = 1;
+                for(int j = 1; j < D; j++) {
+                    if(tMap[j-1][i] == tMap[j][i]) cnt++;
+                    else cnt = 1;
+    
+                    if(cnt == K) {
+                        isOk = true;
+                        break;
+                    }
                 }
+                if(!isOk) break;
             }
-
-            if(!isOk) break;
-        }
-
-        if(isOk) {
-            ans = count;
-            return ;
-        }
-
-        for(int i = d+1; i < D; i++) {
-            find(i, 0, count+1, tMap);
-            find(i, 1, count+1, tMap);
+    
+            if(isOk) {
+                ans = count;
+                return ;
+            }
+    
+            for(int i = d+1; i < D; i++) {
+                for(int j = 0; j < W; j++) tMap[i][j] = 0;
+                find(i, count + 1);
+                for(int j = 0; j < W; j++) tMap[i][j] = map[i][j];
+            }
         }
     }
 }
